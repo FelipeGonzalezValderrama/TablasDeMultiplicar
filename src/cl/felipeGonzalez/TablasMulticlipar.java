05a21edc960f012ed6d9bdba1f350baf15b0694f
Felipe Gonzalez <felipe.gonzalez.valderrama@gmail.com>
2022-10-18 11:02:03 -0300

    first commit

---
+++ src/cl/felipeGonzalez/TablasMulticlipar.java
@@ -0,0 +1,25 @@
+package cl.felipeGonzalez;
+
+import java.util.Scanner;
+
+public class TablasMulticlipar {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int tabla;
+		int i;
+
+		for (tabla = 1; tabla <=20; tabla++) {
+
+			System.out.println("/n/ntabla del " + tabla);
+			System.out.println("---------------------");
+			for (i = 1; i <=20; i++) {
+				System.out.println(tabla + " x " + i + " = "+tabla*i);
+
+			}
+		}
+
+	}
+
+}
